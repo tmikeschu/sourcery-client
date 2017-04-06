016fb8f430b26e2b6ee958f656d9113e53238d4f
Mike Schutte <tmikeschutte@gmail.com>
2017-04-06 14:20:57 -0600

    Set up Nightwatch.js for end-to-end testing
    Add tests for happy and sad path product search
    Update readme instructions
    Add package.json scripts for testing
    
    [Finishes #143287419]

---
+++ e2e-tests/e2e_test.js
@@ -0,0 +1,35 @@
+var config = require('../nightwatch.conf.js');
+
+module.exports = { // adapted from: https://git.io/vodU0
+  'Sourcery Assert Title': browser => {
+    browser
+    .url('http://sourcery-client.herokuapp.com/')
+    .waitForElementVisible('body', 1000)
+    .assert.title('Sourcery')
+    .end();
+  },
+
+  'Bad address input shows sorry message': browser => {
+    browser
+    .url('http://sourcery-client.herokuapp.com/')
+    .waitForElementVisible('body', 1000)
+    .setValue('.product-search input[type=text]', 'badaddress')
+    .click('.product-search input[type=submit]')
+    .assert.containsText('.product-search p', 'Sorry, no results.')
+    .end();
+  },
+
+  'Good address input shows list of paths': browser => {
+    browser
+    .url('http://sourcery-client.herokuapp.com/')
+    .waitForElementVisible('body', 1000)
+    .setValue('.product-search input[type=text]', '0x7da07904ffproduct9afdecb7b1eb0d767b37803')
+    .click('.product-search input[type=submit]')
+    .assert.containsText('.product-search li:nth-of-type(1) h4', 'Created by: Nick')
+    .assert.containsText('.product-search li:nth-of-type(1) button', 'view')
+    .assert.containsText('.product-search > button', 'View All')
+    .assert.elementPresent('.product-search li:nth-of-type(2)')
+    .assert.elementPresent('.product-search li:nth-of-type(3)')
+    .end();
+  }
+};
